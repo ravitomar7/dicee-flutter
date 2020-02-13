bcd3e2eec32ab17d405d60876d08ca2b0630d138
Ravi Tomar <ravitomar7@gmail.com>
2020-02-13 11:17:02 +0530

    Usage of Expanded Class, flex and Stateful Class. Complete Dice App

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
+    DicePage(),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDice, rDice;
+
+  void fun() {
+    lDice = Random().nextInt(6) + 1;
+    rDice = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice$lDice.png'),
+                    onPressed: () {
+                      setState(() {
+                        fun();
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice$rDice.png'),
+                    onPressed: () {
+                      setState(() {
+                        fun();
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
